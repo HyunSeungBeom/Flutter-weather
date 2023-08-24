08592ad8cea20d1fae9836c17f2aed05a0332a7b
현승범 <ghfk100@naver.com>
2023-08-24 23:49:51 +0900

    flutter weather openweather api 연결 작업

--- flutter_weather/lib/services/weather_api.dart
+++ flutter_weather/lib/services/weather_api.dart
@@ -1,1 +1,18 @@
-class WeatherApi {}
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class WeatherApi {
+  final String apiKey = 'c11ddfb2df27f2a833273820ddb26c6e';
+  final String baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  Future<Map<String, dynamic>> getWeatherByCity(String city) async {
+    final response = await http.get('$baseUrl?q=$city&appid=$apiKey' as Uri);
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      return jsonData;
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+}
